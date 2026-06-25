106b5021f5f8f29888fd238de1004062f276f62c
Samuel gonçalves <76127743+StkaaPa@users.noreply.github.com>
2026-06-25 17:18:23 +0100

    Update main.py

--- PROJETO2/main.py
+++ PROJETO2/main.py
@@ -1,6 +1,7 @@
 from src.extract import extract
 from src.transform import transform
 from src.load import (load_stg, load_target)
+from src.p_control import (insert_p_control_proj2)
 from src.log import logger
 
 try:
@@ -10,13 +11,30 @@ try:
 
     load_stg(df)
 
+    rows_read = len(df)
+
     df = transform(df)
 
+    rows_loaded = len(df)
+
     load_target(df)
+
+    insert_p_control_proj2(
+        rows_read,
+        rows_loaded,
+        "SUCCESS"
+    )
 
     logger.info("ETL concluído com sucesso!")
 
 except Exception as e:
     logger.error(str(e))
+
+    insert_p_control_proj2(
+        0,
+        0,
+        "ERROR",
+        str(e)
+    )
 
     raise
