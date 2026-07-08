c118ea2dc812ddc6ff41f82caa3320b422172439
Samuel gonçalves <76127743+StkaaPa@users.noreply.github.com>
2026-07-08 17:24:58 +0100

    Update first_dag.py

--- PROJETO3/dags/first_dag.py
+++ PROJETO3/dags/first_dag.py
@@ -2,3 +2,24 @@ from datetime import datetime
 
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id="first_dag",
+    start_date=datetime(2026,6,16),
+    schedule=None,
+    catchup=False,
+    tags=["learning"],
+) as dag:
+    inicio = EmptyOperator(
+        task_id="inicio"
+    )
+
+    preparar=EmptyOperator(
+        task_id="preparar"
+    )
+
+    fim=EmptyOperator(
+        task_id="fim"
+    )
+
+    inicio>>preparar>>fim
