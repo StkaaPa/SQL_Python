d54b263c7a561044ea59208d18a21f5f1498062f
Samuel gonçalves <76127743+StkaaPa@users.noreply.github.com>
2026-07-11 18:29:22 +0100

    Update load.py

--- PROJETO3/src/load.py
+++ PROJETO3/src/load.py
@@ -2,15 +2,15 @@ from src.database import get_engine
 
 def carregar_csv(**context):
     ti=context["ti"]
-    carregarcsv=ti.xcom_pull(task_ids="transformar_csv")
+    df=ti.xcom_pull(task_ids="transformar_csv")
 
     engine = get_engine()
 
-    carregarcsv.to_sql(
+    df.to_sql(
         name="stg_clientes_proj3",
         con=engine,
         if_exists="append",
         index=False
     )
     
-    print(f"{len(carregarcsv)} registos carregado para staging_clientes_proj3")
+    print(f"{len(df)} registos carregado para staging_clientes_proj3.")
