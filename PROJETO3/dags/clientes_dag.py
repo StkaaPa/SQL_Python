5e0d84ac171d45960388e7de32d306a15c5fdb52
Samuel gonçalves <76127743+StkaaPa@users.noreply.github.com>
2026-07-11 12:11:01 +0100

    Update Clientes_dag.py

--- PROJETO3/dags/clientes_dag.py
+++ PROJETO3/dags/clientes_dag.py
@@ -1,22 +1,19 @@
 from src.extract import ler_csv
 from src.validation import validar_csv
+from src.load import carregar_csv
+from src.transform import transformar_csv
 from datetime import datetime
 
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
-
-def caminho_csv(**context):
-    ti=context["ti"]
-    directory=ti.xcom_pull(task_ids="validar_csv")
-    print(f"O caminho é: {directory}")
     
 with DAG(
     dag_id="clientes_dag",
-    start_date=datetime(2026,08,01),
+    start_date=datetime(2026,8,1),
     schedule=None,
     catchup=False,
-    tags=["Project3"],
+    tags=["Project3", "clientes"],
 ) as dag:
 
     inicio=EmptyOperator(
@@ -24,12 +21,27 @@ with DAG(
     )
 
     validar = PythonOperator(
-        task_id="Validar_csv",
+        task_id="validar_csv",
+        python_callable=validar_csv,
+    )
+
+    extrair = PythonOperator(
+        task_id="ler_csv",
         python_callable=ler_csv,
+    )
+
+    transformar = PythonOperator(
+        task_id="transformar_csv",
+        python_callable=transformar_csv,
+    )
+
+    carregar=PythonOperator(
+        task_id="carregar_csv",
+        python_callable=carregar_csv
     )
 
     fim=EmptyOperator(
         task_id="fim"
     )
 
-inicio>>extrair>>fim
+inicio>>validar>>extrair>>transformar>>carregar>>fim
