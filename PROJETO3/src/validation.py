fd790317d503802cb030a8af95317e7f51c69336
Samuel gonçalves <76127743+StkaaPa@users.noreply.github.com>
2026-07-13 23:22:57 +0100

    Update validation.py

--- PROJETO3/src/validation.py
+++ PROJETO3/src/validation.py
@@ -1,2 +1,14 @@
+from pathlib import Path
+
 def validar_csv():
-    print("Validation OK!")
+    caminho = Path("/opt/airflow/data/clientes.csv")
+
+    if not caminho.exists():
+        raise FileNotFoundError(f"O ficheiro {caminho} não existe.")
+
+    if caminho.stat().st_size == 0:
+        raise ValueError("O ficheiro está vazio.")
+
+    print(f"Ficheiro encontrado: {caminho}")
+
+    return str(caminho)
