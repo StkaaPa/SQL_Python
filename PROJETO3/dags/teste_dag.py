6a84d22ed6fe5124b01b8ed182631cc83e482284
Samuel gonçalves <76127743+StkaaPa@users.noreply.github.com>
2026-07-13 23:21:33 +0100

    Update teste_dag.py

--- PROJETO3/dags/teste_dag.py
+++ PROJETO3/dags/teste_dag.py
@@ -4,17 +4,19 @@ from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
 
-#from src.validation import validar_csv
+from src.validation import validar_csv
+from src.extract import ler_csv
+from src.transform import transformar_csv
+from src.load import carregar_csv
 
-def validar_csv():
-    print("Validation OK!")
 
 with DAG(
-    dag_id="teste_dag",
-    start_date=datetime(2026, 8, 1),
+    dag_id="clientes_dag",
+    start_date=datetime(2026, 1, 1),
     schedule=None,
     catchup=False,
-):
+    tags=["Projeto3", "ETL"],
+) as dag:
 
     inicio = EmptyOperator(
         task_id="inicio"
@@ -25,8 +27,23 @@ with DAG(
         python_callable=validar_csv,
     )
 
+    extrair = PythonOperator(
+        task_id="ler_csv",
+        python_callable=ler_csv,
+    )
+
+    transformar = PythonOperator(
+        task_id="transformar_csv",
+        python_callable=transformar_csv,
+    )
+
+    carregar = PythonOperator(
+        task_id="carregar_csv",
+        python_callable=carregar_csv,
+    )
+
     fim = EmptyOperator(
         task_id="fim"
     )
 
-    inicio >> validar >> fim
+    inicio >> validar >> extrair >> transformar >> carregar >> fim
